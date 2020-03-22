199afed3715912d387f6a38111d5120c73770a3a
Dave Rothfarb <dave_rothfarb@dfci.harvard.edu>
2020-03-22 12:09:31 -0400

    Setting up for covid dashboard

--- home.php
+++ home.php
@@ -9,6 +9,39 @@ get_header ();
 			<?php  dynamic_sidebar('Home carousel') ; ?>
 		</div>
  
+<style>
+  iframe {
+    border: none;
+  }
+  .covid-dashboard {
+    width: 100%;
+    height: 700px;
+  }
+
+  @media (min-width: 768px) and (max-width: 991px) {
+    .covid-dashboard {
+      height: 1000px;
+    }
+  }
+  @media (min-width: 376px) and (max-width: 767px) {
+    .covid-dashboard {
+      height: 1700px;
+    }
+  }
+  @media (max-width: 375px) {
+    .covid-dashboard {
+      height: 1900px;
+    }
+  }
+</style>
+<?php 
+  $url = $_SERVER['SERVER_NAME']; 
+  $path = $_SERVER['REQUEST_URI']; 
+?>
+<iframe 
+    class="covid-dashboard py-3" 
+    src="http://covid-dashboard.hccstaging.com?current_site=<?php echo $url; ?>&current_path=<?php echo $path; ?>">
+</iframe>
 	
 <div class="container py-2 py-md-5 py-lg-4">
 <!-- Home text -->	
